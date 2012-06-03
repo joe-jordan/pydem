9674cb68742cccd919da513af49d3a92eddc6086
Joe Jordan <joe.jordan@imperial.ac.uk>
2012-06-03 21:55:19 +0100

    completing system generation functionality.

--- pydem/granular.py
+++ pydem/granular.py
@@ -21,9 +21,16 @@
 #
 
 import pydem as d
+import pydem.dem as l
+import random, time, math, os
 
 class DepositionMethod:
   RANDOM_SPACED_SHEETS = 0
+
+def rand_float(minimum, maximum):
+  from_system = random.random()
+  scale = maximum - minimum
+  return minimum + scale * from_system
 
 
 def generate_element_line(options, simulation_options, existing_elements):
@@ -40,13 +47,13 @@ def generate_element_line(options, simulation_options, existing_elements):
   
   x_pos = 0.0
   previous_radius = 0.0
-
+  
   new_elements = []
-
+  
   while True:
     new_radius = rand_float(min_radius, max_radius)
     gap_width = gap_scale * rand_float(max_radius / 5.0, max_radius)
-
+    
     if (x_pos + previous_radius + (2.0 * new_radius) + gap_width) < x_limit:
       x_pos += previous_radius + new_radius + gap_width
       new_elements.append(d.Particle({
@@ -57,7 +64,7 @@ def generate_element_line(options, simulation_options, existing_elements):
       previous_radius = new_radius
     else:
       break
-
+  
   return new_elements
 
 def generate_element_sheet(options, simulation_options, existing_elements):
@@ -101,8 +108,6 @@ def generate_element_sheet(options, simulation_options, existing_elements):
         break
   
   return new_elements
-  
-  
 
 def generate_elements(options, simulation_options, existing_elements):
   if options['deposition_method'] == DepositionMethod.RANDOM_SPACED_SHEETS:
@@ -113,8 +118,57 @@ def generate_elements(options, simulation_options, existing_elements):
   else:
     raise Error('cannot use unknown deposition method.')
 
+def fill_criterion(data):
+  vertical = 1
+  key = 'y_limit'
+  if data['params']['dimension'] == 3:
+    vertical = 2
+    key = 'z_limit'
+  return max([e['position'][vertical] for e in data['elements']]) + 4.0 > data['params'][key]
+
+def modulus(v):
+  return math.sqrt(sum([i**2 for i in v]))
+
+def run_to_equilibrium(simulation, system):
+  """now run to equilibrium by limiting particle velocities allow the system to
+relax."""
+  vertical_key = 'y_limit'
+  if system['params']['dimension'] == 3:
+    vertical_key = 'z_limit'
+  
+  theoretical_max_velocity = math.sqrt(2.0 * modulus(system['params']['force_model']['gravity']) * system['params'][vertical_key])
+  
+  current_max_velocity = max([modulus(e['velocity']) for e in system['elements']])
+  travel_limit =  current_max_velocity / 10.0 * system['params']['force_model']['timestep']
+  
+  # our exist condition is that the travel limit (based on the current system KE)
+  # is equivalent to 10^-15 of the velocity obtained by dropping from the top of
+  # the container; the max possible in the system. 
+  while travel_limit > theoretical_max_velocity * (10.0 ** -15) * system['params']['force_model']['timestep']:
+    simulation.limit_velocities(travel_limit)
+    simulation.run_time(1)
+    initial_kinetic_energy = starting_kinetic_energy = simulation.compute_energy(l.Simulation.KINETIC)
+    
+    while True:
+      simulation.run_time(3.0)
+      new_kinetic_energy = simulation.compute_energy(l.Simulation.KINETIC)
+      
+      # when the rate of energy loss starts slowing, drop the velocity limit more. 
+      if abs(starting_kinetic_energy - new_kinetic_energy) < (initial_kinetic_energy / 100):
+        break
+      
+      starting_kinetic_energy = new_kinetic_energy
+    
+    current_max_velocity = max([modulus(e['velocity']) for e in system['elements']])
+    
+    travel_limit =  current_max_velocity / 10.0 * system['params']['force_model']['timestep']
 
 def generate_stable_pack(params=None):
+  """Generates a granular pack in equilibrium. arguments:
+  params = a dict of options for the simulation. read the source to see what
+           defaults are there to override - there are lots, all at the top.
+this function *returns* the new system - the caller must save it to disk. 
+"""
   options = {
     'simulation_params' : {
       'dimension' : 3,
@@ -146,31 +200,45 @@ def generate_stable_pack(params=None):
         options[paramset][key] = value
   
   dimension = options['simulation_params']['dimension']
+  vertical_index = 1
+  vertical_key = 'y_limit'
+  
+  if dimension == 3:
+    vertical_index = 2
+    vertical_key = 'z_limit'
+  else:
+    del options['simulation_params']['z_limit']
+    
   
   # update the force model's container_height from simulation params:
-  options['force_model_params']['container_height'] = options['simulation_params']['y_limit'] if dimension == 2 else options['simulation_params']['z_limit']
+  options['force_model_params']['container_height'] = options['simulation_params'][vertical_key]
   
   # generate the first row/layer of grains:
   elements = generate_elements(options['element_generation_params'], options['simulation_params'], [])
-  data = {'elements' : elements}
+  system = {'elements' : elements}
   
   # construct the simulation params instance and the force model in one fell swoop.
-  data['params'] = d.SimulationParams(options['simulation_params'], options['force_model_params'], data)
+  system['params'] = d.SimulationParams(options['simulation_params'], options['force_model_params'], system)
   
-  s = l.Simulation(data)
+  simulation = l.Simulation(system)
   
-  # TODO.
+  start = time.clock()
+  simulation.run_time(5.0)
   
+  # fill the box
+  while not fill_criterion(system):
+    # TODO update generate call.
+    simulation.add_particles(generate_elements(options['element_generation_params'], options['simulation_params'], system['elements']))
+    simulation.run_time(5.0)
   
+  # now allow to settle to low velocities (note the comparison is on v^2..):
+  while max([sum([val ** 2 for val in e['velocity']]) for e in system['elements']]) > 0.000001:
+    simulation.run_time(20.0)
   
+  run_to_equilibrium(simulation, system)
   
+  end = time.clock()
   
+  system['params']['time_to_completion'] = end - start
   
-  
-  
-  
-  
-  
-  
-  
-  
+  return system
