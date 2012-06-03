7e62890dcacbb67c32394148dbb1ac0aa867d634
Joe Jordan <joe.jordan@imperial.ac.uk>
2012-06-03 21:55:39 +0100

    updating example script, now much simpler.

--- test_run.py
+++ test_run.py
@@ -1,122 +1,62 @@
 import pydem as d
-import pydem.dem as l
-import pydem.simple_visualiser as v
-import pprint, random, math
+import pydem.granular as g
 
-def rand_float(minimum, maximum):
-  from_system = random.random()
-  scale = maximum - minimum
-  return minimum + scale * from_system
-
-
-def generate_elements(min_radius=0.5, max_radius=1.0, y_line=6.0, x_limit=7.0, gap_scale=1.0):
-  x_pos = 0.0
-  previous_radius = 0.0
-
-  new_elements = []
-
-  while True:
-    new_radius = rand_float(min_radius, max_radius)
-    gap_width = gap_scale * rand_float(max_radius / 5.0, max_radius)
-
-    if (x_pos + previous_radius + (2.0 * new_radius) + gap_width) < x_limit:
-      x_pos += previous_radius + new_radius + gap_width
-      new_elements.append(d.Particle({
-        'position': [x_pos, y_line],
-        'radius': new_radius,
-        'mass' : (math.pi * new_radius ** 2)
-      }))
-      previous_radius = new_radius
-    else:
-      break
-
-  return new_elements
-
-def total_energy(data):
-  # potential (from heights):
-  pe = sum([e['position'][1]*e['mass'] for e in data['elements']]) * abs(data['params']['force_model']['gravity'][1])
-  
-  # TODO potential (from overlaps) (requires contact detection to be efficient for large systems...):
-  
-  # kinetic:
-  ke = sum([sum([v_i ** 2 for v_i in e['velocity']]) * e['mass'] for e in data['elements']]) * 0.5
-  # TODO rotational kinetic
-  
-  return ke + pe
-
-
-limits = [50.0, 50.0]
-
-elements = generate_elements(y_line=13.0, x_limit=limits[0])
-
-data = {'elements':elements}
-
-fm = {
-  'type' : d.ForceModelType.HOOKIAN,
-  'resitiution_coefficient' : 0.2,
-  'max_overlap_ratio' : 0.1,
-  'collision_time_ratio' : 100.0,
-  'include_tangential_forces' : True,
-  'container_height' : limits[1]
+# sample options for a monodisperse 3D simulation in a shallow square box.
+#
+# (note that monodispersity combined with this deposition method tends to give
+# the system enough energy to crystallise in spite of tangential friction.)
+demo_3d_pack_options = {
+  'simulation_params' : {
+    'dimension' : 3,
+    'x_limit' : 100.0,
+    'y_limit' : 100.0,
+    'z_limit' : 30.0
+  },
+  'force_model_params' : {
+    'type' : d.ForceModelType.HOOKIAN,
+    'resitiution_coefficient' : 0.1,
+    'max_overlap_ratio' : 0.1,
+    'include_tangential_forces' : True,
+    'tangential_ratio' : 0.7
+  },
+  'element_generation_params' : {
+    'min_radius' : 1.0,
+    'max_radius' : 1.0,
+    'deposition_method' : g.DepositionMethod.RANDOM_SPACED_SHEETS,
+    'separation_scaling' : 1.0
+  }
 }
 
-data['params'] = d.SimulationParams({
-  'dimension' : 2,
-  'x_limit' : limits[0],
-  'y_limit' : limits[1],
-  'max_particles_guess' : 200
-}, fm, data)
 
-s = l.Simulation(data)
+# sample options for a 2D simulation in a square box.
+demo_2d_pack_options = {
+  'simulation_params' : {
+    'dimension' : 2,
+    'x_limit' : 50.0,
+    'y_limit' : 50.0
+  },
+  'force_model_params' : {
+    'type' : d.ForceModelType.HOOKIAN,
+    'resitiution_coefficient' : 0.5,
+    'max_overlap_ratio' : 0.01,
+    'include_tangential_forces' : True,
+    'tangential_ratio' : 0.9
+  },
+  'element_generation_params' : {
+    'min_radius' : 0.5,
+    'max_radius' : 1.0,
+    'deposition_method' : g.DepositionMethod.RANDOM_SPACED_SHEETS,
+    'separation_scaling' : 0.5
+  }
+}
 
-use_graphics = True
+print "generating 2d pack..."
 
-if use_graphics:
-  r = v.SimulationRenderer(data, 20)
-  frame_rate = 50.0
-  frame_time = 1.0 / frame_rate
+system = g.generate_stable_pack(demo_2d_pack_options)
 
+print "done! saving to disk."
 
+d.save_system(system, 'stable_2d_pack')
 
-# note, this wrapper assumes time is seconds not num of timesteps.
-def run_time(time):
-  if use_graphics:
-    run_time = 0.0
-    
-    while (run_time < time):
-      s.run_time(frame_time)
-      run_time += frame_time
-      r.render(data)
-  else:
-    s.run_time(time)
-
-run_time(5.0)
-
-# fill the box
-
-while max([e['position'][1] for e in data['elements']]) + 4.0 < limits[1]:
-  print "ran successfully, now adding more elements"
-  
-  print "energy before adding new elements:", total_energy(data)
-  
-  s.add_particles(generate_elements(y_line=max([e['position'][1] for e in data['elements']])+3.0, x_limit=limits[0]))
-  
-  print "energy after adding new elements:", total_energy(data)
-  
-  run_time(5.0)
-
-# now run to equillibrium
-
-print "running to equillibrium, as small velocity."
-
-while max([e['velocity'][0] ** 2 + e['velocity'][1] ** 2 for e in data['elements']]) > 0.00001:
-  run_time(20.0)
-
-
-d.save_system(data, 'stable_pack')
-
-
-
-
-
+print "exiting..."
 
